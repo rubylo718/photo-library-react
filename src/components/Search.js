a951d8df9d811e3ab850fa7e68c93472149d664d
rubylo718 <rubylo718@gmail.com>
2023-04-03 15:57:29 +0800

    feat: add axios to fetch data from pexeles api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
+	let [data, setData] = useState(null)
+	const auth = process.env.REACT_APP_PEXELS_AUTH;
+	const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+
+	const search = async () => {
+		let result = await axios.get(initialURL, {
+			headers: { Authorization: auth },
+		});
+		setData(result.data.photos)
+	};
+
 	return (
 		<div className="search">
 			<input className="input" type="text" />
-			<button>Search</button>
+			<button onClick={search}>Search</button>
 		</div>
 	);
 };
