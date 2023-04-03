9ffb4fdb1a60bbf41d79cd2dc9c38912f5641e26
rubylo718 <rubylo718@gmail.com>
2023-04-03 21:04:46 +0800

    feat: add load more function to fetch more photos

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,8 +4,10 @@ import Search from "../components/Search";
 import Picture from "../components/Picture";
 
 const Homepage = () => {
-	let [input, setInput] = useState("")
+	let [input, setInput] = useState("");
 	let [data, setData] = useState(null);
+	let [page, setPage] = useState(1);
+	let [currentSearch, setCurrentSearch] = useState("")
 	const auth = process.env.REACT_APP_PEXELS_AUTH;
 	const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
 	let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
@@ -15,6 +17,21 @@ const Homepage = () => {
 			headers: { Authorization: auth },
 		});
 		setData(result.data.photos);
+		setCurrentSearch(input)
+	};
+
+	const morePicture = async () => {
+		let newURL;
+		setPage(page + 1);
+		if (currentSearch === "") {
+			newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+		} else {
+			newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page + 1}`;
+		}
+		let result = await axios.get(newURL, {
+			headers: { Authorization: auth },
+		});
+		setData(data.concat(result.data.photos));
 	};
 
 	useEffect(() => {
@@ -23,14 +40,20 @@ const Homepage = () => {
 
 	return (
 		<div style={{ minHeight: "100vh" }}>
-			<Search search={() => {
-				search(searchURL)
-			}} setInput={setInput}/>
+			<Search
+				search={() => {
+					search(searchURL);
+				}}
+				setInput={setInput}
+			/>
 			<div className="pictures">
 				{data &&
 					data.map((d) => {
 						return <Picture key={d.id} data={d} />;
 					})}
+			</div>
+			<div className="morePicture">
+				<button onClick={morePicture}>更多圖片</button>
 			</div>
 		</div>
 	);
