0a2bce2ddd82a7f863dc7d4928c9117d1abd772f
rubylo718 <rubylo718@gmail.com>
2023-04-03 17:14:38 +0800

    feat: add component Picture and show pictures in Homepage

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,6 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
-const Search = () => {
-	let [data, setData] = useState(null)
-	const auth = process.env.REACT_APP_PEXELS_AUTH;
-	const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-
-	const search = async () => {
-		let result = await axios.get(initialURL, {
-			headers: { Authorization: auth },
-		});
-		setData(result.data.photos)
-	};
-
+const Search = ({ search }) => {
 	return (
 		<div className="search">
 			<input className="input" type="text" />
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Search from "../components/Search";
+import Picture from "../components/Picture";
 
 const Homepage = () => {
+	let [data, setData] = useState(null);
+	const auth = process.env.REACT_APP_PEXELS_AUTH;
+	const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+
+	const search = async () => {
+		let result = await axios.get(initialURL, {
+			headers: { Authorization: auth },
+		});
+		console.log(result.data.photos);
+		setData(result.data.photos);
+	};
+
 	return (
 		<div style={{ minHeight: "100vh" }}>
-			<Search />
+			<Search search={search} />
+			<div className="pictures">
+				{data &&
+					data.map((d) => {
+						return <Picture key={d.id} data={d} />;
+					})}
+			</div>
 		</div>
 	);
 };
